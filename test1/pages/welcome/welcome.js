e768f46125a58ce9252dbf238f2489cf1111de31
YunLambert <469066072@qq.com>
2018-02-14 00:48:21 +0800

    增加了movies页面

--- test1/pages/welcome/welcome.js
+++ test1/pages/welcome/welcome.js
@@ -1,10 +1,10 @@
 /***需要加个动画效果，点击事件发生后会有个动画效果然后到程序界面上***/
 Page({
   onTap:function(){
-    wx.navigateTo({url:"../posts/post"})
-  //  wx.redirectTo({
-  //    url:"../posts/post"
-  //   })
+    wx.switchTab({url:"../posts/post"})
+    // wx.redirectTo({
+    //  url:"../posts/post"
+    // })
   },
 
   onUnload:function(){
