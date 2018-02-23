6736a63e2a33377edb0f2ab7f6a20d51020de268
YunLambert <469066072@qq.com>
2018-02-23 22:06:23 +0800

    电影页面数据绑定完成

--- test1/pages/movies/movies.js
+++ test1/pages/movies/movies.js
@@ -3,17 +3,22 @@ var app = getApp();
 Page({
   //RESTFUL API JSON
   //SOAP XML WSDL
+  data:{
+    inTheaters:{},
+    comingSoon:{},
+    top250:{}
+  },
 
   onLoad: function () {
     var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
     var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
     var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
 
-    this.getMovieListData(inTheatersUrl);
-    // this.getMovieListData(comingSoonUrl);
-    // this.getMovieListData(top250Url);
+    this.getMovieListData(inTheatersUrl,"inTheaters");
+    this.getMovieListData(comingSoonUrl,"comingSoon");
+    this.getMovieListData(top250Url,"top250");
   },
-  getMovieListData: function (url) {
+  getMovieListData: function (url,settedKey) {
     var that = this;
     wx.request({
       url: url,
@@ -24,14 +29,14 @@ Page({
       success: function (res) {
         console.log(res),
         console.log("success");
-        that.processDoubanData(res.data)
+        that.processDoubanData(res.data,settedKey)
       },
       fail: function (error) {
           console(error)
       }
     })
   },
-  processDoubanData:function(moviesDouban){
+  processDoubanData: function (moviesDouban, settedKey){
     var movies=[];
     for(var idx in moviesDouban.subjects){
       var subject = moviesDouban.subjects[idx];
@@ -47,8 +52,10 @@ Page({
       }
       movies.push(temp)
     }
-    this.setData({
+    var readyData = {};
+    readyData[settedKey] = {
       movies:movies
-    })
+      }
+    this.setData(readyData);
   }
 })
