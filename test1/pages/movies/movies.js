87e7a8ec4ab43cfc4ef4a41c8df7f1e48b5bf8d8
YunLambert <469066072@qq.com>
2018-02-24 15:53:00 +0800

    样式表文件还没写好

--- test1/pages/movies/movies.js
+++ test1/pages/movies/movies.js
@@ -3,10 +3,10 @@ var app = getApp();
 Page({
   //RESTFUL API JSON
   //SOAP XML WSDL
-  data:{
-    inTheaters:{},
-    comingSoon:{},
-    top250:{}
+  data: {
+    inTheaters: {},
+    comingSoon: {},
+    top250: {}
   },
 
   onLoad: function () {
@@ -14,10 +14,19 @@ Page({
     var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
     var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
 
-    this.getMovieListData(inTheatersUrl,"inTheaters","正在热映");
-    this.getMovieListData(comingSoonUrl,"comingSoon","即将上映");
-    this.getMovieListData(top250Url,"top250","豆瓣Top250");
+    this.getMovieListData(inTheatersUrl, "inTheaters", "正在热映");
+    this.getMovieListData(comingSoonUrl, "comingSoon", "即将上映");
+    this.getMovieListData(top250Url, "top250", "豆瓣Top250");
   },
+
+  onMoreTap: function (event) {
+    var category = event.currentTarget.dataset.category;
+    wx.navigateTo({
+      url: "more-movie/more-movie?category=" + category
+    })
+  },
+
+
   getMovieListData: function (url, settedKey, categoryTitle) {
     var that = this;
     wx.request({
@@ -28,36 +37,37 @@ Page({
       },
       success: function (res) {
         console.log(res),
-        console.log("success");
+          console.log("success");
         that.processDoubanData(res.data, settedKey, categoryTitle)
       },
       fail: function (error) {
-          console(error)
+        console(error)
       }
     })
   },
-  processDoubanData: function (moviesDouban, settedKey,categoryTitle){
-    var movies=[];
-    for(var idx in moviesDouban.subjects){
+
+  processDoubanData: function (moviesDouban,settedKey,categoryTitle) {
+    var movies = [];
+    for (var idx in moviesDouban.subjects) {
       var subject = moviesDouban.subjects[idx];
-      var title=subject.title;
-      if(title.length>=6){
-        title = title.substring(0,6)+"...";
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "...";
       }
-      var temp={
-        stars:util.convertToStarsArray(subject.rating.stars),
-        title:title,
-        average:subject.rating.average,
-        coverageUrl:subject.images.large,
-        movieId:subject.id
+      var temp = {
+        stars: util.convertToStarsArray(subject.rating.stars),
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
       }
       movies.push(temp)
     }
-    var readyData = {};
-    readyData[settedKey] = {
-      categoryTitle:categoryTitle,
-      movies:movies
-      }
-    this.setData(readyData);
-  }
+      var readyData = {};
+      readyData[settedKey] = {
+        categoryTitle:categoryTitle,
+        movies:movies
+        }
+      this.setData(readyData);
+    },
 })
